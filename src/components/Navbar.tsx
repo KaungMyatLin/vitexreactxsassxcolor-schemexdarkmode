ca9f0a782b3a1a66f30b2ea342c690bc419cccb5
KaungMyatLin <kaungmyatlin.worklife@gmail.com>
2022-12-03 00:31:53 +0630

    u

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import {useContext} from 'react'
-import { ThemeContext } from '../context/ThemeContext'
-import classes from './navbar.module.scss'
+import {useContext} from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+import classes from './navbar.module.scss';
+
 export const Navbar = () => {
     const {theme} = useContext(ThemeContext)
     document.documentElement.style.colorScheme = theme
+
+    let style:string = theme === 'light'? classes.light: classes.dark;
     return (
-        <nav className={`${classes.nav}`}>
+        <nav className={`${classes.nav} ${style}`}>
             <h1> Logo </h1>
             <ul>
                 <li>About</li>
